1c15762ec4f7cbb994556f0d030c16217eea1729
vtocchio <80677157+viniciustocchio@users.noreply.github.com>
2022-08-01 11:28:55 +0200

    added models

---
+++ models.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+let movieSchema = mongoose.Schema({
+  Title: { type: String, require: true },
+  Description: { type: String, required: true },
+  Genre: {
+    Name: String,
+    Description: String,
+  },
+  Director: {
+    Name: String,
+    Bio: String,
+  },
+  Actors: [String],
+  ImagePath: String,
+  Featired: Boolen,
+});
+
+let userSchema = mongoose.Schema({
+  Username: { type: String, require: true },
+  Password: { type: String, require: true },
+  Email: { type: String, require: true },
+  Birthday: Date,
+  FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, re: "Movie" }],
+});
+
+let Movie = mongoose.model("Movie", moviesSchema);
+let User = mongoose.model("User", userSchema);
+
+module.exports.Movies = Movie;
+module.exports.User = User;
