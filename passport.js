6c5d7fffa55b44d4d3ea0f7233c44dece21173b3
vtocchio <80677157+viniciustocchio@users.noreply.github.com>
2022-09-14 14:01:52 +0200

    small changes

--- passport.js
+++ passport.js
@@ -15,7 +15,7 @@ passport.use(
     },
     (username, password, callback) => {
       console.log(username + "  " + password);
-      Users.findOne({ username: username }, (error, user) => {
+      Users.findOne({ Username: username }, (error, user) => {
         if (error) {
           console.log(error);
           return callback(error);
