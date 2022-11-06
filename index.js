d0548b4325e936a4220c395781766afc0b0dd390
vtocchio <80677157+viniciustocchio@users.noreply.github.com>
2022-11-06 10:38:28 +0100

    commented cors

--- index.js
+++ index.js
@@ -12,26 +12,26 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const cors = require("cors");
 
 let allowedOrigins = [
-  "http://localhost:8080",
-  "http://localhost:1234",
-  "http://testsite.com",
-  "http://localhost:4200",
+//   "http://localhost:8080",
+//   "http://localhost:1234",
+//   "http://testsite.com",
+//   "http://localhost:4200",
 ];
 
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        let message =
-          "The CORS policy for this application doesn’t allow access from origin " +
-          origin;
-        return callback(new Error(message), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         let message =
+//           "The CORS policy for this application doesn’t allow access from origin " +
+//           origin;
+//         return callback(new Error(message), false);
+//       }
+//       return callback(null, true);
+//     },
+//   })
+// );
 
 let auth = require("./auth")(app);
 
