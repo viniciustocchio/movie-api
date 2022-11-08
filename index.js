734a454fb3bf299fb02ad30e938fa7c2a1d12c35
vtocchio <80677157+viniciustocchio@users.noreply.github.com>
2022-11-08 11:10:20 +0100

    added app.get

--- index.js
+++ index.js
@@ -181,6 +181,21 @@ app.get("/documentation", (req, res) => {
 
 app.use(express.static("public"));
 
+// app.get(
+//   "/users/:Username",
+//   passport.authenticate("jwt", { session: false }),
+//   (req, res) => {
+//     Users.findOne({ Username: req.params.Username })
+//       .then((user) => {
+//         res.json(user);
+//       })
+//       .catch((err) => {
+//         console.error(err);
+//         res.status(500).send("Error: " + err);
+//       });
+//   }
+// );
+
 app.get('/users/:Username/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
   Users.findOne({ Username: req.params.Username })
       .then((user) => {
